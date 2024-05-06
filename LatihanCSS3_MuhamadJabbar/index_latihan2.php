e1f427f6169a44c927e983e0a7a35424f95506bc
JB-ID <mjbarl831@gmail.com>
2024-05-06 15:48:54 +0700

    Latihan CSS 3

---
+++ LatihanCSS3_MuhamadJabbar/index_latihan2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Card</title>
+
+    <link rel="stylesheet" href="style_latihan2.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-img">
+                <img src="https://i.pinimg.com/originals/ea/a6/98/eaa698b63df39e7c9cad20ba9b35d0c8.jpg" alt="F1 Car">
+            </div>
+
+            <div class="card-body">
+                <h2 class="card-tittle">Drive to Survive (2022)</h2>
+                <p class="card-intro">
+                    Drive to survive is content about F1 paddock and problem documentary about..
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container2">
+        <div class="card2">
+            <div class="card-img2">
+                <img src="https://c4.wallpaperflare.com/wallpaper/961/486/336/formula-1-formula-cars-red-bull-racing-mercedes-f1-hd-wallpaper-thumb.jpg" alt="F1 Car">
+            </div>
+
+            <div class="card-body2">
+                <h2 class="card-tittle2">Drive to Survive (2023)</h2>
+                <p class="card-intro2">
+                    Drive to survive is content about F1 paddock and problem documentary about..
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container3">
+        <div class="card3">
+            <div class="card-img3">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrcJYvaJtLoj8AkvaAWUJ1gTaQx7Xyuo3s4V1cYbmZrg&s" alt="F1 Car">
+            </div>
+
+            <div class="card-body3">
+                <h2 class="card-tittle3">Drive to Survive (2024)</h2>
+                <p class="card-intro3">
+                    Drive to survive is content about F1 paddock and problem documentary about..
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
